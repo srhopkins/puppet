956d54416fa57523c2191972ddadbf339c094b84
Steven Hopkins <srhopkins@gmail.com>
2015-07-16 21:50:10 +0000

    updated spacing

--- graphite_centos7/manifests/init.pp
+++ graphite_centos7/manifests/init.pp
@@ -36,30 +36,29 @@
 # Copyright 2015 Your name here, unless otherwise noted.
 #
 class graphite_centos7 {
-
-	$packages = [
-	"epel-release",
-	"python-pip",
-	"httpd",
-	"net-snmp",
-	"perl",
-	"pycairo",
-	"mod_wsgi",
-	"python-devel",
-	"git",
-	"gcc-c++",
-	"npm", ]
-	package { $packages: ensure => "installed", }
-
-	$pip_packages = [
-	"https://github.com/graphite-project/ceres/tarball/master",
-	"django<1.6",
-	"Twisted<12",
-	"django-tagging==0.3.6",
-	"whisper",
-	"graphite-web",
-	"carbon", ]
-	package { $pip_packages:
+  $packages = [
+    "epel-release",
+    "python-pip",
+    "httpd",
+    "net-snmp",
+    "perl",
+    "pycairo",
+    "mod_wsgi",
+    "python-devel",
+    "git",
+    "gcc-c++",
+    "npm", ]
+  package { $packages: ensure => "installed", }
+  
+  $pip_packages = [
+    "https://github.com/graphite-project/ceres/tarball/master",
+    "django<1.6",
+    "Twisted<12",
+    "django-tagging==0.3.6",
+    "whisper",
+    "graphite-web",
+    "carbon", ]
+  package { $pip_packages:
     ensure   => "installed",
     provider => "pip",
     require  => Package[$packages],
