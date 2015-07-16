d7dcebaf7b219dc19c417fc5c6b49e5ed0e6a3e2
Steven Hopkins <srhopkins@gmail.com>
2015-07-16 19:45:30 +0000

    Converted to classwq

--- graphite-centos7.pp
+++ graphite-centos7.pp
@@ -1,27 +1,31 @@
-$packages = [ 
-"epel-release",
-"python-pip",
-"httpd",
-"net-snmp",
-"perl",
-"pycairo",
-"mod_wsgi",
-"python-devel",
-"git",
-"gcc-c++",
-"npm", ]
-package { $packages: ensure   => "installed", }
+class graphite-centos7 {
 
-$pip_packages = [ 
-"https://github.com/graphite-project/ceres/tarball/master",
-"django<1.6",
-"Twisted<12",
-"django-tagging==0.3.6",
-"whisper",
-"graphite-web",
-"carbon", ]
-package { $pip_packages:
-	ensure   => "installed",
-	provider => "pip",
-	require  => Package[$packages],
+	$packages = [
+	"epel-release",
+	"python-pip",
+	"httpd",
+	"net-snmp",
+	"perl",
+	"pycairo",
+	"mod_wsgi",
+	"python-devel",
+	"git",
+	"gcc-c++",
+	"npm", ]
+	package { $packages: ensure => "installed", }
+
+	$pip_packages = [
+	"https://github.com/graphite-project/ceres/tarball/master",
+	"django<1.6",
+	"Twisted<12",
+	"django-tagging==0.3.6",
+	"whisper",
+	"graphite-web",
+	"carbon", ]
+	package { $pip_packages:
+			ensure   => "installed",
+			provider => "pip",
+			require  => Package[$packages],
+	}
+
 }
